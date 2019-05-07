5c34996c065fd6eb1aba9b73caf3ad3d41732a77
zuphzuph <zuphzuph@users.noreply.github.com>
2019-05-07 11:39:53 -0600

    Update MSSQL-To-SMTP.ps1

--- MSSQL-To-SMTP.ps1
+++ MSSQL-To-SMTP.ps1
@@ -1,10 +1,10 @@
 ﻿import-module sqlps;
-$myData = invoke-sqlcmd -InputFile "Full Path to .SQL Query" -serverinstance -InstanceName -database DBToRunOn;
+$mydata = invoke-sqlcmd -InputFile "Full Path to .SQL Query" -serverinstance -InstanceName -database DBToRunOn;
 $mydata | out-file SaveLocationForQueryResults.sql;
 remove-module sqlps;
 
-$Username = "domain/acct";
-$Password = "cred";
+$username = "domain/acct";
+$password = "cred";
 $path = "Path of Log Result to Send_$(get-date -f MM-dd).txt";
 
 function Send-ToEmail([string]$email, [string]$attachmentpath){
@@ -18,10 +18,10 @@ function Send-ToEmail([string]$email, [string]$attachmentpath){
     $message.Attachments.Add($attachment);
     $smtp = new-object Net.Mail.SmtpClient("smtp.domain.com");
     $smtp.EnableSSL = $true;
-    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($username, $password);
     [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { return $true }
     $smtp.send($message);
     write-host "Mail Sent" ; 
     $attachment.Dispose();
  }
-Send-ToEmail  -email "destination.domain.com" -attachmentpath $path;
+Send-ToEmail  -email "destination@domain.com" -attachmentpath $path;
